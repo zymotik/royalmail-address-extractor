7ab11f4edd14fb8a46396ea7b06e9868e4d0f0b8
Ian Brown <ian.brown@iress.com>
2021-08-21 16:33:37 +0100

    add house and business name support

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,12 @@
 (function() {
     const listSelector = 'pcalist';
     const itemSelector = 'pcaitem';
-    const numberRegex = /(^[0-9]+\w*)|(^\w+[0-9]*\s(\d|\w)[,]?\s[0-9]+\w*)/; /* match: "7", "12A", "Apartment 4, 23", "Flat A, 1" (Fix: does not match house or business names) */
+    /*RegEx match: "7", "12A", "Apartment 4, 23", "Flat A, 1", "Any amount of words until first comma" */
+    const numberRegex = /(^[0-9]+\w*)|(^\w+[0-9]*\s(\d|\w)[,]?\s[0-9]+\w*)|(^.+?(?=,))/;
 
     function mapElementAttributeToNumber(element) {
-        return element.getAttribute('title').match(numberRegex)[0];
+        const matches = element.getAttribute('title').match(numberRegex);
+        return matches && matches.length > 0 && matches[0];
     }
 
     function format(list) {
@@ -14,8 +16,9 @@
     function extractAddresses() {
         return Array.from(
             document.getElementsByClassName(listSelector)[0]
-                    .getElementsByClassName(itemSelector))
-                    .map(mapElementAttributeToNumber).filter(v => !!v);
+                    .getElementsByClassName(itemSelector)
+                    ).map(mapElementAttributeToNumber)
+                     .filter(v => !!v);
     }
 
     alert(format(extractAddresses()));
