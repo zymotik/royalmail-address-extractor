eb507b97782b019f146c96c3d73ec4966dec7fa9
Ian Brown <ian.brown@iress.com>
2021-08-21 16:15:45 +0100

    support apartments and flats in regex

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,10 @@
 (function() {
     const listSelector = 'pcalist';
     const itemSelector = 'pcaitem';
-    const numberRegex = /[0-9]*\w*/;
+    const numberRegex = /(^[0-9]+\w*)|(^\w+[0-9]*\s(\d|\w)[,]?\s[0-9]+\w*)/; /* match: "7", "12A", "Apartment 4, 23", "Flat A, 1" (Fix: does not match house or business names) */
 
     function mapElementAttributeToNumber(element) {
-        return element.getAttribute('title').match(numberRegex);
+        return element.getAttribute('title').match(numberRegex)[0];
     }
 
     function format(list) {
